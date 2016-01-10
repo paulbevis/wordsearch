ac69f31e45f1e3a62ba71b14e191a559d4fcb98f
Paul Bevis <paulbevis2010@gmail.com>
2016-01-10 21:57:10 +1000

    Words randomly placed on board, and either go horizontally or vertically, in a random manner

--- reducers/gamePlay.js
+++ reducers/gamePlay.js
@@ -5,12 +5,17 @@ import {GAMES} from '../constants/data'
 
 const CHAR_SET = "abcdefghijklmnopqrstuvwxyz";
 const START_SET = "-";
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
+const LEFT_TO_RIGHT = 'LEFT_TO_RIGHT';
+const TOP_TO_BOTTOM = 'TOP_TO_BOTTOM';
+const WORD_DIRECTIONS = [LEFT_TO_RIGHT, TOP_TO_BOTTOM];
 
-function fillDefaultGrid(cells, gridSet) {
-    for (var i = 0; i < 6; i++) {
+export function fillDefaultGrid(cells, gridSet) {
+    for (var i = 0; i < GRID_WIDTH; i++) {
         var row = [];
         cells.push({row});
-        for (var j = 0; j < 6; j++) {
+        for (var j = 0; j < GRID_HEIGHT; j++) {
             var cell = new Cell(gridSet.charAt(Math.floor(Math.random() * gridSet.length)), i, j, false);
             row.push(cell);
         }
@@ -22,10 +27,10 @@ function generateGameGrid(action) {
     let game = GAMES[action.changeValue];
     if (game) {
         let cells = [];
-        for (var i = 0; i < 6; i++) {
+        for (var i = 0; i < GRID_WIDTH; i++) {
             var row = [];
             cells.push({row});
-            for (var j = 0; j < 6; j++) {
+            for (var j = 0; j < GRID_HEIGHT; j++) {
                 var cell = new Cell(game.board[i][j], i, j, false);
                 row.push(cell);
             }
@@ -73,8 +78,6 @@ function wordsFoundUpdate(newState, state, action) {
 
         let wordFound = newState.lettersFound.reduce((prev, curr) => prev + curr.value, '');
 
-        console.log('letters found as a word: ', wordFound)
-
         let posOfMatchedWord = state.words.findIndex(word=> wordFound === word.word && !word.wordFound)
 
         if (posOfMatchedWord !== -1) {
@@ -87,14 +90,13 @@ function wordsFoundUpdate(newState, state, action) {
                 letterCell.partOFWordFound = true;
                 letterCell.selected = false;
             });
-            newState.lettersFound=[];
+            newState.lettersFound = [];
         }
     }
 }
 
 
 export function gamePlay(state = initialState, action) {
-    console.log('game play here')
     let newState = {};
     switch (action.type) {
         case CELL_CLICK:
@@ -103,14 +105,10 @@ export function gamePlay(state = initialState, action) {
                 cellSelectedUpdate(newState, state, action);
 
                 //lettersFound update
-                lettersFoundUpdate(newState, state, action)
-                //newState.lettersFound = [];
-
-                //words update
-                //newState.words = state.words;
+                lettersFoundUpdate(newState, state, action);
 
                 //wordsFound update
-                wordsFoundUpdate(newState, state, action)
+                wordsFoundUpdate(newState, state, action);
 
                 return newState;
             } else {
@@ -118,8 +116,8 @@ export function gamePlay(state = initialState, action) {
             }
 
         case GAME_SELECT:
-            newState.cells = generateGameGrid(action);
-            newState.words = words(action);
+            //newState.cells = generateGameGrid(action);
+            words(newState, action);
             newState.lettersFound = [];
 
             return newState;
@@ -129,14 +127,59 @@ export function gamePlay(state = initialState, action) {
     }
 }
 
-function words(action) {
-    let words = [];
+function addWordToGrid(newState, word) {
+    let wordDirection = Math.floor(Math.random() * 2);
+
+    let wordPositions = [];
+    let columnAddition = 1;
+    let rowAddition = 0;
+    if (WORD_DIRECTIONS[wordDirection] == TOP_TO_BOTTOM) {
+        columnAddition = 0;
+        rowAddition = 1;
+    }
+    let wordComplete = 0;
+    //repeat until either the word is placed or it gives up on a 1000 iterations
+    while (wordPositions.length !== word.word.length && wordComplete < 1000) {
+        //random starting point
+        let columnPos = Math.floor(Math.random() * GRID_WIDTH);
+        let rowPos = Math.floor(Math.random() * GRID_HEIGHT);
+        //reset words found in grid
+        wordPositions = [];
+        // cycle through word, character by character
+        word.word.split('').map(character=> {
+            // if location is untaken, or matches exact letter, then continue
+            if (columnPos < GRID_WIDTH && rowPos < GRID_HEIGHT &&
+                (newState.cells[columnPos].row[rowPos].value === '-' || (newState.cells[columnPos].row[rowPos].value === character))) {
+                wordPositions.push({'letter': character, 'colPosition': columnPos, 'rowPosition': rowPos});
+                columnPos = columnPos + columnAddition;
+                rowPos = rowPos + rowAddition;
+            }
+            wordComplete++;
+        });
+    }
+
+    // now that word mapped, mark the grid.
+    if (wordPositions.length == word.word.length) {
+        wordPositions.map(characterPosition=>
+            newState.cells[characterPosition.colPosition].row[characterPosition.rowPosition].value = characterPosition.letter
+        )
+    } else{
+        console.log("FAILED!!!!!!!!!!!!!!!!!")
+    }
+
+    return wordPositions;
+}
+
+function words(newState, action) {
+    newState.cells = fillDefaultGrid([], START_SET);
+    newState.words = [];
     let game = GAMES[action.changeValue];
     game.words.map(wordStr=> {
         let word = {};
         word.word = wordStr;
         word.wordFound = false;
-        words.push(word);
+        word.positionInGrid = addWordToGrid(newState, word);
+        newState.words.push(word);
     });
     return words;
 }
