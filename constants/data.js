1c0486c16692e3792d801dc9f521ca9f344986ea
Paul Bevis <paulbevis2010@gmail.com>
2016-01-12 21:58:28 +1000

    New word grids added

--- constants/data.js
+++ constants/data.js
@@ -1,37 +1,103 @@
 export const GAMES = {
+    //GOLD
     'wordSet1': {
-        'words': ['one', 'two', 'three', 'four','five','six','seven','eight','nine','ten'],
-        'board': [
-            ['o', 'n', 'e', '-', '-', '-'],
-            ['t', 'w', 'o', '-', '-', '-'],
-            ['t', 'h', 'r', 'e', 'e', '-'],
-            ['-', '-', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-']
-        ]
+        'words': ['and', 'is', 'of', 'the', 'in', 'it', 'like', 'we']
     },
 
+    //RED
     'wordSet2': {
-        'words': ['four', 'five', 'six'],
-        board: [
-            ['f', 'o', 'u', 'r', '-', '-'],
-            ['f', 'i', 'v', 'e', '-', '-'],
-            ['s', 'i', 'x', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-']
-        ]
+        'words': ['was', 'can', 'at', 'so', 'he', 'see', 'to', 'am', 'on', 'up']
     },
 
+    //BLUE
     'wordSet3': {
-        'words': ['seven', 'eight', 'nine'],
-        board: [
-            ['s', 'e', 'n', '-', '-', '-'],
-            ['e', 'i', 'i', '-', '-', '-'],
-            ['v', 'g', 'n', '-', '-', '-'],
-            ['e', 'h', 'e', '-', '-', '-'],
-            ['n', 't', '-', '-', '-', '-'],
-            ['-', '-', '-', '-', '-', '-']
-        ]
+        'words': ['no', 'me', 'you', 'my', 'an', 'do', 'go', 'all', 'are', 'as']
+    },
+
+    //GREEN
+    'wordSet4': {
+        'words': ['for', 'had', 'her', 'his', 'not', 'they', 'by', 'if', 'or', 'did']
+    },
+
+    //ORANGE
+    'wordSet5': {
+        'words': ['said', 'has', 'him', 'now', 'out', 'she', 'that', 'this', 'well', 'went']
+    },
+
+    //INDIGO
+    'wordSet6': {
+        'words': ['come', 'then', 'will', 'look', 'boy', 'day', 'girl', 'us', 'jump', 'play']
+    },
+
+    //VIOLET
+    'wordSet7': {
+        'words': ['old', 'rad', 'read', 'saw', 'how', 'say', 'to', 'but', 'have', 'get']
+    },
+
+    //PINK
+    'wordSet8': {
+        'words': ['with', 'back', 'into', 'new', 'off', 'want', 'our', 'been', 'there', 'their']
+    },
+
+    //PURPLE
+    'wordSet9': {
+        'words': ['put', 'who', 'came', 'down', 'from', 'where', 'just', 'made', 'much', 'over']
+    },
+
+    //AQUA
+    'wordSet10': {
+        'words': ['them', 'when', 'here', 'make', 'were', 'what', 'your', 'about', 'before', 'could']
+    },
+
+    //LIME1
+    'wordSet11': {
+        'words': ['one', 'two', 'little', 'eat', 'after', 'five', 'help', 'home', 'good', 'give']
+    },
+
+    //LIME2
+    'wordSet12': {
+        'words': ['mother', 'room', 'away', 'fast', 'let', 'four', 'going', 'take', 'because', 'any']
+    },
+
+    //LEMON1
+    'wordSet13': {
+        'words': ['why', 'than', 'time', 'year', 'very', 'know', 'tell', 'three', 'under', 'must']
+    },
+
+    //LEMON2
+    'wordSet14': {
+        'words': ['only', 'some', 'more', 'other', 'done', 'right', 'which', 'rain', 'almost', 'ride']
+    },
+
+    //RUBY1
+    'wordSet15': {
+        'words': ['hide', 'night', 'use', 'again', 'ask', 'these', 'water', 'work', 'city', 'write']
+    },
+
+    //RUBY2
+    'wordSet16': {
+        'words': ['father', 'once', 'would', 'gave', 'place', 'behind', 'world', 'sky', 'book', 'black']
+    },
+
+    //JADE1
+    'wordSet17': {
+        'words': ['should', 'blue', 'left', 'round', 'always', 'walk', 'today', 'anything', 'week', 'both']
+    },
+
+    //JADE2
+    'wordSet18': {
+        'words': ['name', 'white', 'morning', 'paper', 'sleep', 'found', 'thing', 'open', 'long', 'small']
+    },
+
+    //PEARL1
+    'wordSet19': {
+        'words': ['people', 'school', 'every', 'another', 'house', 'door', 'window', 'across', 'stay', 'last']
+    },
+
+    //PEARL2
+    'wordSet20': {
+        'words': ['able', 'first', 'think', 'find', 'own', 'something', 'become', 'end', 'head', 'soon']
     }
+
+
 };
