8d00ccd6b4eac63c7264da79286ee02c559832a3
Paul Bevis <paulbevis2010@gmail.com>
2016-01-21 07:27:14 +1000

    Grid now pads rows top and bottom, when using a smaller grid

--- js/components/Grid.js
+++ js/components/Grid.js
@@ -1,15 +1,35 @@
 import React, { Component, PropTypes } from 'react'
 import Row from './Row'
+import {MAX_GRID_HEIGHT} from '../constants/data'
 
 export default class Grid extends Component {
+    provideDummyRows(keyPefix) {
+        let gridHeight = this.props.grid.rows.length;
+        let blankRow = MAX_GRID_HEIGHT - gridHeight;
+        if (blankRow !== 0) {
+            blankRow = (blankRow / 2)
+        }
+        var divs = [];
+        let counter = 0;
+        while (counter < blankRow) {
+            divs.push(<div key={keyPefix+counter} className='section group'>
+                <div className="col span_12_of_12" style={{height:'40px'}}></div>
+            </div>);
+            counter++;
+        }
+        return divs
+    }
+
     render() {
         return (
             <div className="col span_9_of_12" style={{background: 'white'}}>
-            <div className="section group" style={{background: 'white'}}>
-                {this.props.grid.rows.map((row, index)=>
-                    <Row key={index} onCellClick={this.props.onCellClick} {...row} />)
-                }
-            </div>
+                <div className="section group" style={{background: 'white'}}>
+                    {this.provideDummyRows('b')}
+                    {this.props.grid.rows.map((row, index)=>
+                        <Row key={index} onCellClick={this.props.onCellClick} {...row} />)
+                    }
+                    {this.provideDummyRows('a')}
+                </div>
             </div>
         )
     }
