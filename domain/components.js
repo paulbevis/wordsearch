71a971c51460ffe7fa4a837325c3dc84e8c7a3ac
Paul Bevis <paulbevis2010@gmail.com>
2016-01-20 22:08:54 +1000

    Code structure altered

--- domain/components.js
+++
@@ -1,8 +0,0 @@
-export class Cell {
-    constructor(value, rowPos, columnPos, selected) {
-        this.value = value;
-        this.rowPos = rowPos;
-        this.columnPos = columnPos;
-        this.selected=selected;
-    }
-}
