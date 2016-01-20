6fd5b357ada0521af2a14d94bd1c7e7557f35bc9
Paul Bevis <paulbevis2010@gmail.com>
2016-01-20 22:13:44 +1000

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,16 @@
 var path = require('path');
 module.exports = {
-  entry: "./entry.js",
-  output: {
-    path: __dirname,
-    filename: "./public/[name].js",
-    chunkFilename: "[id].js"
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loaders: [ 'babel' ],
-        exclude: /node_modules/,
-        include: __dirname
-      }
-    ]
-  }
+    entry: "./entry.js",
+    output: {
+        path: __dirname,
+        filename: "./public/[name].js",
+        chunkFilename: "[id].js"
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: "style!css"},
+            {test: /\.js$/, loaders: ['babel'], exclude: /node_modules/, include: __dirname}
+        ]
+    }
 };
