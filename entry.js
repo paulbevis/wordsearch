858bdaf1dc1f348f00926a2e9536acc70f6dac65
Paul Bevis <paulbevis2010@gmail.com>
2016-01-20 22:09:12 +1000

    Code tidy

--- entry.js
+++ entry.js
@@ -2,14 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import WordSearch from './containers/wordSearch'
-//import configureStore from './store/configureStore'
 
 
 import { createStore } from 'redux'
 import wordSearchAppReducers from './reducers/wordSearch'
 const store = createStore(wordSearchAppReducers);
-
-//const store = configureStore()
 
 render(
   <Provider store={store}>
