66420211b76289b614bce55df9d432670d525e0b
Paul Bevis <paulbevis2010@gmail.com>
2016-01-25 08:44:43 +1000

    Grid size attached to specific boards

--- js/constants/data.js
+++ js/constants/data.js
@@ -4,139 +4,141 @@ export const GAMES = {
     //GOLD
     'wordSet1': {
         'words': ['and', 'is', 'of', 'the', 'in', 'it', 'like', 'we'],
-        gridWidth:6,
-        gridHeight:6
+        gridWidth: 6,
+        gridHeight: 6
     },
 
     //RED
     'wordSet2': {
         'words': ['was', 'can', 'at', 'so', 'he', 'see', 'to', 'am', 'on', 'up'],
-        gridWidth:6,
-        gridHeight:6
+        gridWidth: 6,
+        gridHeight: 6
     },
 
     //BLUE
     'wordSet3': {
         'words': ['no', 'me', 'you', 'my', 'an', 'do', 'go', 'all', 'are', 'as'],
-        gridWidth:6,
-        gridHeight:6
+        gridWidth: 6,
+        gridHeight: 6
     },
 
     //GREEN
     'wordSet4': {
         'words': ['for', 'had', 'her', 'his', 'not', 'they', 'by', 'if', 'or', 'did'],
-        gridWidth:6,
-        gridHeight:6
+        gridWidth: 6,
+        gridHeight: 6
     },
 
     //ORANGE
     'wordSet5': {
         'words': ['said', 'has', 'him', 'now', 'out', 'she', 'that', 'this', 'well', 'went'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //INDIGO
     'wordSet6': {
         'words': ['come', 'then', 'will', 'look', 'boy', 'day', 'girl', 'us', 'jump', 'play'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //VIOLET
     'wordSet7': {
         'words': ['old', 'rad', 'read', 'saw', 'how', 'say', 'to', 'but', 'have', 'get'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //PINK
     'wordSet8': {
         'words': ['with', 'back', 'into', 'new', 'off', 'want', 'our', 'been', 'there', 'their'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //PURPLE
     'wordSet9': {
         'words': ['put', 'who', 'came', 'down', 'from', 'where', 'just', 'made', 'much', 'over'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //AQUA
     'wordSet10': {
         'words': ['them', 'when', 'here', 'make', 'were', 'what', 'your', 'about', 'before', 'could'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //LIME1
     'wordSet11': {
         'words': ['one', 'two', 'little', 'eat', 'after', 'five', 'help', 'home', 'good', 'give'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //LIME2
     'wordSet12': {
         'words': ['mother', 'room', 'away', 'fast', 'let', 'four', 'going', 'take', 'because', 'any'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //LEMON1
     'wordSet13': {
         'words': ['why', 'than', 'time', 'year', 'very', 'know', 'tell', 'three', 'under', 'must'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //LEMON2
     'wordSet14': {
-        'words': ['only', 'some', 'more', 'other', 'done', 'right', 'which', 'rain', 'almost', 'ride']
+        'words': ['only', 'some', 'more', 'other', 'done', 'right', 'which', 'rain', 'almost', 'ride'],
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //RUBY1
     'wordSet15': {
         'words': ['hide', 'night', 'use', 'again', 'ask', 'these', 'water', 'work', 'city', 'write'],
-        gridWidth:8,
-        gridHeight:8
+        gridWidth: 8,
+        gridHeight: 8
     },
 
     //RUBY2
     'wordSet16': {
         'words': ['father', 'once', 'would', 'gave', 'place', 'behind', 'world', 'sky', 'book', 'black'],
-        gridWidth:10,
-        gridHeight:10
+        gridWidth: 10,
+        gridHeight: 10
     },
 
     //JADE1
     'wordSet17': {
         'words': ['should', 'blue', 'left', 'round', 'always', 'walk', 'today', 'anything', 'week', 'both'],
-        gridWidth:10,
-        gridHeight:10
+        gridWidth: 10,
+        gridHeight: 10
     },
 
     //JADE2
     'wordSet18': {
         'words': ['name', 'white', 'morning', 'paper', 'sleep', 'found', 'thing', 'open', 'long', 'small'],
-        gridWidth:10,
-        gridHeight:10
+        gridWidth: 10,
+        gridHeight: 10
     },
 
     //PEARL1
     'wordSet19': {
-        'words': ['people', 'school', 'every', 'another', 'house', 'door', 'window', 'across', 'stay', 'last'] ,
-        gridWidth:10,
-        gridHeight:10
+        'words': ['people', 'school', 'every', 'another', 'house', 'door', 'window', 'across', 'stay', 'last'],
+        gridWidth: 10,
+        gridHeight: 10
     },
 
     //PEARL2
     'wordSet20': {
         'words': ['able', 'first', 'think', 'find', 'own', 'something', 'become', 'end', 'head', 'soon'],
-        gridWidth:10,
-        gridHeight:10
+        gridWidth: 10,
+        gridHeight: 10
     }
 
 
