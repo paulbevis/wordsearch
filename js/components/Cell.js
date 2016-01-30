2b5d19898405ad3c993b36bccf27b7d1cd4e65e2
Paul Bevis <paulbevis2010@gmail.com>
2016-01-30 13:54:14 +1000

    checks for new function call

--- js/components/Cell.js
+++ js/components/Cell.js
@@ -36,5 +36,6 @@ export default class Cell extends Component {
 }
 
 Cell.propTypes = {
-    onClick: PropTypes.func.isRequired
+    onClick: PropTypes.func.isRequired,
+    onCellExplosionFragment: PropTypes.func.isRequired
 };
