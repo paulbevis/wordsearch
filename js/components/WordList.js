64fbce6597563cc336041b851f3dcb7e9a77166a
Paul Bevis <paulbevis2010@gmail.com>
2016-01-30 13:54:00 +1000

    changes to explosion colours and padding of the game areas

--- js/components/WordList.js
+++ js/components/WordList.js
@@ -18,7 +18,7 @@ export default class WordList extends Component {
         };
         return (
             <div className="col span_3_of_12" style={style}>
-                <div className="section group">
+                <div className="section group" style={{padding: '5px 0px'}}>
                     {
                         this.props.wordList.map((word, index)=>
                             <Word key={word.word+index} {...word}/>)
