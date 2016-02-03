cf0466210ec62fdfd19aad8287d242b5e59d025a
Paul Bevis <paulbevis2010@gmail.com>
2016-02-03 19:17:05 +1000

    Added license messaging

--- js/reducers/game-play.js
+++ js/reducers/game-play.js
@@ -1,3 +1,18 @@
+/*
+ Copyright 2015 Paul Bevis
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 import {CELL_CLICK, GAME_START, GAME_SELECT, CELL_EXPLOSION_FRAGMENT, LAST_LETTER_FOUND} from '../constants/action-types'
 import {MAX_GRID_WIDTH, MAX_GRID_HEIGHT, START_SET} from '../constants/data'
 import createBoard,{fillDefaultGrid} from './board-creator'
