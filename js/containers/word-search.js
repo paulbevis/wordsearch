56d0529a00123fd5170842ea0418cd64b3a7db8d
Paul Bevis <paulbevis2010@gmail.com>
2016-04-19 07:16:38 +1000

    The game now always starts at list 1, and processes from there.  You cannot choose the word lists anyone, to stop the children jumping about

--- js/containers/word-search.js
+++ js/containers/word-search.js
@@ -13,73 +13,72 @@
  See the License for the specific language governing permissions and
  limitations under the License.
  */
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
+import React, {Component, PropTypes} from 'react'
+import {connect} from 'react-redux'
 import {cellClickAction, startGameAction, selectGameAction, cellExplosionFragmentAction, lastLetterFoundAction} from '../actions/word-search'
 import Grid from '../components/grid'
-import Start from '../components/start'
 import WordList from '../components/word-list'
 import PlaySound from '../components/play-sound'
-import SelectGameDialog from '../components/select-game-dialog'
 import Toolbar from 'material-ui/lib/toolbar/toolbar';
-import FlatButton from 'material-ui/lib/flat-button';
-import RaisedButton from 'material-ui/lib/raised-button';
-import AboutGameDialog from '../components/about-dialog'
+import ToolbarGroup from 'material-ui/lib/toolbar/toolbar-group';
+import RaisedButton from 'material-ui/lib/raised-button';import AboutGameDialog from '../components/about-dialog'
 
 class WordSearch extends Component {
 
-    componentDisplay() {
-        let container = [];
+  componentDisplay() {
+    let container = [];
 
-        container.push(<Grid key="grid" onCellClick={(rowPos,columnPos) =>this.props.dispatch(cellClickAction(rowPos, columnPos))}
-                             grid={this.props.grid}
-                             onCellExplosionFragment={(rowPos,columnPos)=>this.props.dispatch(cellExplosionFragmentAction(rowPos,columnPos))}
-                             onLastLetterOfLastWord={()=>this.props.dispatch(lastLetterFoundAction())}
-                             gameOver={this.props.gameOver}/>)
-        container.push(<WordList key="wordList" wordList={this.props.wordList}/>)
-        return container
-    }
+    container.push(<Grid key="grid" onCellClick={(rowPos,columnPos) =>this.props.dispatch(cellClickAction(rowPos, columnPos))}
+                         grid={this.props.grid}
+                         onCellExplosionFragment={(rowPos,columnPos)=>this.props.dispatch(cellExplosionFragmentAction(rowPos,columnPos))}
+                         onLastLetterOfLastWord={()=>this.props.dispatch(lastLetterFoundAction())}
+                         gameOver={this.props.gameOver}/>)
+    container.push(<WordList key="wordList" wordList={this.props.wordList}/>)
+    return container
+  }
 
-    render() {
-        const { dispatch, grid, selectedGame, wordList, sound, gameOver } = this.props;
+  render() {
+    const {dispatch, grid, selectedGame, wordList, sound, gameOver} = this.props;
 
-        return (
-            <div>
-                <div className="section group">
-                    <div className="col span_8_of_12" style={{fontSize: '25px', lineHeight: '38px', color: 'white', paddingLeft: '10px'}}>Year One Sight Words</div>
-                    <div className="col span_3_of_12" style={{fontSize: '22px', lineHeight: '38px',  color:'#333', marginLeft:'30px'}}><span style={{float: 'right'}}>{selectedGame.description}</span></div>
-                </div>
-                <div id="root" className="section group" style={{ }}>
-                    <div className="col span_12_of_12" style={{marginTop: '3px', marginBottom: '0'}}>
+    return (
+      <div>
+        <div className="section group">
+          <div className="col span_8_of_12" style={{fontSize: '25px', lineHeight: '38px', color: 'white', paddingLeft: '10px'}}>Year One Sight Words</div>
+          <div className="col span_3_of_12" style={{fontSize: '22px', lineHeight: '38px',  color:'#333', marginLeft:'30px'}}><span style={{float: 'right'}}>{selectedGame.description}</span></div>
+        </div>
+        <div id="root" className="section group" style={{ }}>
+          <div className="col span_12_of_12" style={{marginTop: '3px', marginBottom: '0'}}>
 
-                        <div className="section group">
-                            {this.componentDisplay()}
-                        </div>
-                    </div>
-                    <div className="section group">
-                        <div className="col span_12_of_12">
-
-                            <Toolbar style={{background:'#0cc3ff'}}>
-                                <SelectGameDialog onGameSelect={(value) => dispatch(selectGameAction(value))}/>
-                                <AboutGameDialog/>
-                            </Toolbar>
-                        </div>
-                    </div>
-                    <PlaySound sound={sound} game={selectedGame.number} nextGame={value => dispatch(selectGameAction(value))}/>
-                </div>
+            <div className="section group">
+              {this.componentDisplay()}
             </div>
-        )
-    }
+          </div>
+          <div className="section group">
+            <div className="col span_12_of_12">
+
+              <Toolbar style={{background:'#0cc3ff'}}>
+                <ToolbarGroup firstChild={true}>
+                  <RaisedButton label="Start" primary={true} onClick={()=>dispatch(selectGameAction(1))}/>
+                </ToolbarGroup>
+                <AboutGameDialog/>
+              </Toolbar>
+            </div>
+          </div>
+          <PlaySound sound={sound} game={selectedGame.number} nextGame={value => dispatch(selectGameAction(value))}/>
+        </div>
+      </div>
+    )
+  }
 }
 
 function select(state) {
-    return {
-        grid: state.gamePlay.grid,
-        selectedGame: state.currentGame,
-        wordList: state.gamePlay.words,
-        sound: state.gamePlay.sound,
-        gameOver: state.gamePlay.gameOver
-    };
+  return {
+    grid: state.gamePlay.grid,
+    selectedGame: state.currentGame,
+    wordList: state.gamePlay.words,
+    sound: state.gamePlay.sound,
+    gameOver: state.gamePlay.gameOver
+  };
 }
 
 // Wrap the component to inject dispatch and state into it
