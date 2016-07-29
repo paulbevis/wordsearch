25afd2b7deb38a251256a08001df8672d8c7f01d
Paul Bevis <paulbevis2010@gmail.com>
2016-07-29 18:05:03 +1000

    Testing different sound file for congratulations

--- js/components/play-sound.js
+++ js/components/play-sound.js
@@ -22,7 +22,7 @@ export default class PlaySound extends Component {
       <div>
         <audio src="audio/success.m4a" preload="auto" ref={(ref) => this.success = ref}/>
         <audio src="audio/warning.m4a" preload="auto" ref={(ref) => this.warning = ref}/>
-        <audio src="audio/applause.m4a" preload="auto" ref={(ref) => this.congratulations = ref}/>
+        <audio src="audio/warning.m4a" preload="auto" ref={(ref) => this.congratulations = ref}/>
       </div>
     )
   }
